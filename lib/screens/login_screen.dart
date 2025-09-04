414fbc1486c62bb6f37c633e6b37ddb40dc81a85
Victor Eduardo Mendoza Lopez <victormendoza2008@outlook.com>
2025-09-04 10:55:52 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,17 +9,71 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _isObscure = true; // Estado inicial
+
   @override
   Widget build(BuildContext context) {
+    // Para obtener el tamaño de la pantalla del disp.
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       // Evita nudge o cámaras frontales para móviles
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: RiveAnimation.asset('assets/animated_login_character.riv'),
-            ),
-          ],
+        child: Padding(
+          // Eje X/horizontal/derecha izquierda
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: RiveAnimation.asset(
+                  'assets/animated_login_character.riv',
+                ),
+              ),
+              // Espacio entre el oso y el texto Emal
+              const SizedBox(height: 10),
+              // Campo de texto del Email
+              TextField(
+                // Para que aparezca el @ en móviles UI/UX
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    // Esquinas redondeadas
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 10),
+              // Campo de texto de la contraseña
+              TextField(
+                // Para ocultar el texto
+                obscureText: _isObscure,
+                // Para que aparezca el @ en móviles UI/UX
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _isObscure = !_isObscure;
+                      });
+                    },
+                    icon: Icon(
+                      _isObscure ? Icons.visibility : Icons.visibility_off,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    // Esquinas redondeadas
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
